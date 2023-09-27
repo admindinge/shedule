a1d6fae7d6e59b137de3b4b8549591ab751c40f3
Avtandil Pangani <126405138+admindinge@users.noreply.github.com>
2023-09-27 23:15:44 +0000

    საბოლოო ფორატირება

--- shedule.py
+++ shedule.py
@@ -1,34 +1,64 @@
+""" ᲡᲙᲝᲚᲘᲡ ᲐᲕᲢᲝᲛᲐᲢᲣᲠᲘ ᲖᲐᲠᲘᲡ ᲒᲐᲜᲠᲘᲒᲘᲡ ᲒᲔᲜᲔᲠᲐᲪᲘᲐ """
 from datetime import datetime, timedelta
 
+
 class SchoolScheduleError(Exception):
-    pass
+    """
+        ᲒᲐᲜᲠᲘᲒᲘᲡ ᲒᲔᲜᲔᲠᲐᲪᲘᲐ ᲨᲔᲪᲓᲝᲛᲐ
+    """
+    # No code needed here
+
 
 class InvalidSchoolCodeError(SchoolScheduleError):
+    """
+        არაკორექტული სკოლის კოდი
+
+        მაგალითად, სკოლის კოდი უნდა იყოს 4 ციფრიანი
+    """
+
     def __init__(self, code):
         self.code = code
-        self.message = f"არაკორექტული სკოლის კოდი: {code}"
+        self.message = f"არაკორექტული სკოლის კოდი: {repr(code)}"
         super().__init__(self.message)
+
 
 class ScheduleGenerationError(SchoolScheduleError):
+    """
+        Განრიგის გენერაციის შეცდომა
+    """
+
     def __init__(self, message):
-        self.message = f"განრიგის გენერაციის შეცდომა: {message}"
+        self.message = f"განრიგის გენერაციის შეცდომა: {repr(message)}"
         super().__init__(self.message)
 
+
 def parse_time(time_str):
-    
-        parsed_time = datetime.strptime(time_str, '%H:%M')
-        if parsed_time.hour < 8 or parsed_time.hour > 15:
-            raise ScheduleGenerationError("გაკვეთილის დაწყების დრო უნდა იყოს  8-დან  15 საათამდე.")
-        if parsed_time.minute < 0 or parsed_time.minute > 59:
-            raise ScheduleGenerationError("წუთების მაჩვენებელი უნდა იყოს  0-59 დიაპაზონში.")
-        return parsed_time
-    
+    """
+        ᲓᲠᲝᲘᲡ ᲤᲝᲠᲛᲐᲢᲘᲡ ᲞᲐᲠᲡᲘᲜᲒᲘ
+    """
+    parsed_time = datetime.strptime(time_str, '%H:%M')
+    if parsed_time.hour < 8 or parsed_time.hour > 15:
+        raise ScheduleGenerationError(
+            "გაკვეთილის დაწყების დრო უნდა იყოს  8-დან  15 საათამდე.")
+    if parsed_time.minute < 0 or parsed_time.minute > 59:
+        raise ScheduleGenerationError(
+            "წუთების მაჩვენებელი უნდა იყოს  0-59 დიაპაზონში.")
+    return parsed_time
+
 
 def get_input_or_default(prompt, default):
+    """
+        ᲞᲐᲠᲐᲛᲔᲢᲠᲘᲡ ᲨᲔᲧᲕᲐᲜᲐ
+    """
     user_input = input(f"{prompt} ({default}): ").strip()
     return default if user_input == "" else user_input
 
+
 def generate_schedule(schedule_params):
+    """
+        Განრიგის გენერაცია
+    """
+
     lessons_count = int(schedule_params['გაკვეთილების რაოდენობა'])
     lesson_duration = int(schedule_params['გაკვეთილის ხანგრძლივობა'])
     break_duration = int(schedule_params['დასვენების ხანგრძლივობა'])
@@ -36,70 +66,74 @@ def generate_schedule(schedule_params):
     big_break_duration = int(schedule_params['დიდი დასვენების ხანგრძლივობა'])
 
     start_time = parse_time(schedule_params['გაკვეთილების დაწყება'])
-    
-    
+
     if start_time is None:
         return []
 
     schedule = []
     current_time = start_time
-    
-    run_program_string = '/home/tc/bell.c'
+
+    run_program_string = 'sudo /home/tc/bell.c'
 
     for lesson_num in range(lessons_count):
-        
 
-        # ᲒᲐᲙᲕᲔᲗᲘᲚᲘᲡ ᲓᲐᲬᲧᲔᲑᲐ
+        # ᲒᲐᲙᲕᲔᲗᲘᲚᲘ {lesson_num + 1}
         schedule.append(f'# ᲒᲐᲙᲕᲔᲗᲘᲚᲘ {lesson_num + 1} ')
-        schedule.append(f'{current_time.strftime("%M %H")} * *  1-5 {run_program_string} in')
-        # ᲒᲐᲙᲕᲔᲗᲘᲚᲘᲡ ᲓᲐᲡᲠᲣᲚᲔᲑᲐ
-        schedule.append( f'{(current_time + timedelta(minutes=lesson_duration)).strftime("%M %H")} * *  1-5 {run_program_string} out')
-        schedule.append(f'')
+        schedule.append(
+            f'{current_time.strftime("%M %H")} * *  1-5 {run_program_string} in')
+        # ᲒᲐᲙᲕᲔᲗᲘᲚᲘ {lesson_num + 1}
+        schedule.append(
+            f'{(current_time + timedelta(minutes=lesson_duration)).strftime("%M %H")} * *  1-5 {run_program_string} out')
+        schedule.append('')
         current_time += timedelta(minutes=lesson_duration)
-
-        
 
         if (lesson_num + 1) == big_break_count and lesson_num < lessons_count - 1:
             # Большая перемена начинается
             current_time += timedelta(minutes=big_break_duration)
 
-        
-        else:    
+        else:
             current_time += timedelta(minutes=break_duration)
 
-        
-
     return schedule
+
+
 def main():
-    
+    """
+        ᲛᲐᲥᲐᲘᲐᲗᲐ ᲒᲐᲜᲠᲘᲒᲘᲡ ᲒᲔᲜᲔᲠᲐᲪᲘᲐ
+    """
     try:
 
+       # ᲡᲙᲝᲚᲘᲡ ᲙᲝᲓᲘᲡ ᲛᲝᲗᲮᲝᲕᲜᲐ
+        school_code = get_input_or_default(
+            'ᲨᲔᲘᲧᲕᲐᲜᲔ ᲡᲙᲝᲚᲘᲡ 4 ᲜᲘᲨᲜᲐ ᲙᲝᲓᲘ', '0000')
 
-        # ᲡᲙᲝᲚᲘᲡ ᲙᲝᲓᲘᲡ ᲛᲝᲗᲮᲝᲕᲜᲐ
-        school_code = get_input_or_default('ᲨᲔᲘᲧᲕᲐᲜᲔ ᲡᲙᲝᲚᲘᲡ 4 ᲜᲘᲨᲜᲐ ᲙᲝᲓᲘ', '0000')
-        
         # ᲡᲙᲝᲚᲘᲡ ᲙᲝᲓᲘᲡ ᲨᲔᲛᲝᲬᲛᲔᲑᲐ
         if not school_code.isdigit() or len(school_code) != 4:
-            raise InvalidSchoolCodeError(school_code)
-        
-        
+            raise InvalidSchoolCodeError(repr(school_code))
+        lesson_count = 'გაკვეთილების რაოდენობა'
+        start_time = 'გაკვეთილების დაწყება'
+        lesson_duration = 'გაკვეთილის ხანგრძლივობა'
+        break_duration = 'დასვენების ხანგრძლივობა'
+        big_break_duration = 'დიდი დასვენების ხანგრძლივობა'
+        big_break_count = 'რომელია დიდი დასვენება'
+
         schedule_params = {
-            'გაკვეთილების რაოდენობა': get_input_or_default('შეიყვანე გაკვეთილების რაოდენობა', '7'),
-            'გაკვეთილების დაწყება': get_input_or_default('შეიყვანე გაკვეთილების დაწყების დრო (ᲡᲡ:ᲬᲬ)', '9:00'),
-            'გაკვეთილის ხანგრძლივობა': get_input_or_default('შეიყვანე გაკვეთილის ხანგრძლივობა', '45'),
-            'დასვენების ხანგრძლივობა': get_input_or_default('შეიყვანე შესვენების ხანგრძლივობა', '5'),
-            'დიდი დასვენების ხანგრძლივობა': get_input_or_default('შეიყვანე დიდი დასვენების ხანგრძლივობა', '10'),
-            'რომელია დიდი დასვენება': get_input_or_default('შეიყვანე რომელია დიდი დასვენება', '3')
+            lesson_count: get_input_or_default('შეიყვანე გაკვეთილების რაოდენობა', '7'),
+            start_time: get_input_or_default('შეიყვანე გაკვეთილის დაწყების დრო (ᲡᲡ:ᲬᲬ)', '9:00'),
+            lesson_duration: get_input_or_default('შეიყვანე გაკვეთილის ხანგრძლივობა', '45'),
+            break_duration: get_input_or_default('შეიყვანე შესვენების ხანგრძლივობა', '5'),
+            big_break_duration: get_input_or_default('შეიყვანე დიდი დასვენების ხანგრძლივობა', '10'),
+            big_break_count: get_input_or_default(
+                'შეიყვანე რომელია დიდი დასვენება', '3')
         }
 
         # ᲒᲐᲜᲠᲘᲒᲘᲡ ᲒᲔᲜᲔᲠᲐᲪᲘᲐ
         schedule = generate_schedule(schedule_params)
 
         # ᲤᲐᲘᲚᲘᲡ ᲡᲐᲮᲔᲚᲘ ᲘᲥᲜᲔᲑᲐ ᲡᲙᲝᲚᲘᲡ ᲙᲝᲓᲘ
-        
 
         # ᲕᲮᲡᲜᲘᲗ ᲤᲐᲘᲚᲡ ᲩᲐᲬᲔᲠᲘᲡᲐᲗᲕᲘᲡ
-        with open(school_code, 'w') as file:
+        with open(school_code, 'w', encoding='utf-8') as file:
             # ᲗᲘᲗᲝᲔᲣᲚ ᲔᲚᲔᲛᲔᲜᲢᲡ ᲕᲬᲔᲠᲗ ᲤᲐᲘᲚᲨᲘ
             for item in schedule:
                 file.write(str(item) + '\n')
@@ -111,18 +145,17 @@ def main():
         print("")
         for item in schedule:
             print(item)
-            
-    except InvalidSchoolCodeError as e:
-        print(f"ᲨᲔᲪᲓᲝᲛᲐ (ᲙᲝᲓᲘ {e.code}): {e.message}")
-        
-    except ScheduleGenerationError as e:
-        print(f"ᲨᲔᲪᲓᲝᲛᲐ: {e.message}")
 
-    except Exception as e:
-        print("ᲨᲔᲪᲓᲝᲛᲐ: ", str(e))
-    
+    except InvalidSchoolCodeError as value_error:
+        print(f"ᲨᲔᲪᲓᲝᲛᲐ (ᲙᲝᲓᲘ {value_error.code}): {value_error.message}")
+
+    except ScheduleGenerationError as value_error:
+        print(f"ᲨᲔᲪᲓᲝᲛᲐ: {value_error.message}")
+
+    except ValueError as value_error:
+        print("ᲨᲔᲪᲓᲝᲛᲐ: ", str(value_error))
+
 
 if __name__ == "__main__":    # Параметры расписания на основе ваших данных
-    
-    main()       
-        
+
+    main()
