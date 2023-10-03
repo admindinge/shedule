ef19bd49dc60617315d1daf3d7dd3d0e888ee532
Avtandil Pangani <126405138+admindinge@users.noreply.github.com>
2023-10-03 19:51:39 +0000

    check

--- check_sedule.sh
+++ check_sedule.sh
@@ -2,13 +2,13 @@
 
 # Скачиваем файл с расписанием с GitHub
 github_file_url="https://raw.githubusercontent.com/admindinge/shedule/main/4751"
-temp_file_path="./temp_4751"
+temp_file_path="./temp"
 curl -sSL "$github_file_url" -o "$temp_file_path"
 
 # Проверяем изменения в файле
 new_hash=$(sha256sum "$temp_file_path" | awk '{print $1}')
-if [ -f "previous_4751" ]; then
-    previous_hash=$(cat "previous_4751")
+if [ -f "previous" ]; then
+    previous_hash=$(cat "previous")
     if [ "$new_hash" == "$previous_hash" ]; then
         echo "ᲤᲐᲘᲚᲘ ᲐᲠ ᲨᲔᲪᲕᲚᲘᲚᲐ"
         exit 0
@@ -16,15 +16,18 @@ if [ -f "previous_4751" ]; then
 fi
 
 # Сохраняем текущий crontab в файл
-# crontab -l > "current_crontab.txt"
+crontab -l > "current_crontab.txt"
 
 # Обновляем crontab из файла temp_file_path
-# crontab "$temp_file_path"
+crontab "$temp_file_path"
 
-# Вызываем filetool.sh для сохранения изменений, включая crontab
-# filetool.sh -b
+
 
 # Обновляем хеш в файле
-echo "$new_hash" > "previous_4751"
+echo "$new_hash" > "previous"
 
 echo "ᲤᲐᲘᲚᲘ ᲨᲔᲪᲕᲚᲘᲚᲘ, ᲒᲐᲜᲐᲮᲚᲔᲑᲣᲚᲘᲐ ᲒᲐᲜᲠᲘᲒᲘᲪ ᲓᲐ ᲮᲔᲨᲘᲪ."
+
+
+# Вызываем filetool.sh для сохранения изменений, включая crontab
+filetool.sh -b
